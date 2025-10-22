fce012b04c8e7eec25c6a97341e4c32006ebbcee
Sai Teja <sidduala2006@gmail.com>
2025-10-23 00:44:35 +0530

    added files

---
+++ template.sh
@@ -0,0 +1,15 @@
+#directories
+mkdir -p src
+mkdir -p resarch
+
+#files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touc resarch/trails.ipynb
+touch requirements.txt
+
+echo "files and directories created"
